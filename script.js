8b57cdc40190c468a44050bc02972ade8e789ecd
elianagsilva <elianacrisa60@gmail.com>
2022-07-08 20:38:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let frutas = ["maçã", "banana", "mamão", "laranja", "uva"]
+console.log(frutas)
+
+frutas.unshift("melão")
+console.log(frutas)
+
+frutas.pop()
+console.log(frutas)
+
+frutas.push("amora", "caju")
+console.log(frutas)
+
+frutas.shift()
+console.log(frutas)
+
+let = "numbers" = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+console.log(numbers)
